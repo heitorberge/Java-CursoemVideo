6eb516daac8b3b8721c53eaedcc265ae6c1389e2
Heitor Berg <heitorberg114@gmail.com>
2025-01-16 14:56:45 -0300

    Delete Aulas CursoEmVideo Java/Aula 06/EntradaDeDados directory

--- Aulas CursoEmVideo Java/Aula 06/EntradaDeDados/src/main/java/com/mycompany/entradadedado/TiposPrimitivos.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.entradadedado;
-import java.util.Scanner;
-public class TiposPrimitivos {
-    public static void main(String[] args) {
-        Scanner teclado = new Scanner (System.in);
-        float nota = teclado.nextFloat();
-        System.out.format("A Nota e %.1f \n", nota);
-    }
-}

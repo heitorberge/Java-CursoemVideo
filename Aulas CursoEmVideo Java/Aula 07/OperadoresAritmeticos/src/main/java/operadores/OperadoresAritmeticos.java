094225108c94ca2494d16cfa4026493dd457c0e7
Heitor Berg <heitorberg114@gmail.com>
2025-01-18 11:15:44 -0300

    Aula 07 Adicionada

---
+++ Aulas CursoEmVideo Java/Aula 07/OperadoresAritmeticos/src/main/java/operadores/OperadoresAritmeticos.java
@@ -0,0 +1,11 @@
+package operadores;
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        // var
+            int n2 = 5;
+            int n1 = 3;
+            float m = (n1 + n2)/2;
+        // fim var
+        System.out.println("A media de " + n1 + " e " + n2 + " e " + m);
+    }
+} 

6c3ddcb394db1fb1e04199abf9d500666778e6b9
Heitor Berg <heitorberg114@gmail.com>
2025-01-21 09:40:16 -0300

    Add files via upload

---
+++ Aulas CursoEmVideo Java/Aula 08/OperadorTernario/src/main/java/cursoemvideo/operadorternario/OperadorTernario.java
@@ -0,0 +1,10 @@
+package cursoemvideo.operadorternario;
+public class OperadorTernario {
+    public static void main(String[] args) {
+        int maior;
+        int n1 = 5;
+        int n2 = 7;
+        maior = n1>n2 ? n1 : n2;
+        System.out.println("A variavel com maior valor possui o valor de: " + maior);
+    }
+}
